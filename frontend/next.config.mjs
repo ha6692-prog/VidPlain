59746f7b6aa2895fd8ad9334fad1204b495af81d
Hiteshagr <hiteshagrawal0098@mail.com>
2026-03-24 14:19:26 +0530

    fix: upgrade next.js v15, downgrade tailwind to v3, fix build issues

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,23 +6,16 @@ const __dirname = path.dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false,
+  output: 'export',
+  trailingSlash: true,
+  devIndicators: {
+    buildActivity: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  turbopack: {
-    root: __dirname,
-  },
   images: {
     unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/django-api/:path*',
-        destination: 'http://127.0.0.1:8001/api/:path*',
-      },
-    ]
   },
 }
 
